7ce4284b79b93362ab5ab113b759fb006ba917bf
GeruN <33115757+GerunNik@users.noreply.github.com>
2018-11-12 14:59:19 +0100

    Add files via upload

---
+++ AtomicTime/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace AtomicTime
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.showTime_txt = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // showTime_txt
+            // 
+            this.showTime_txt.AutoSize = true;
+            this.showTime_txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F);
+            this.showTime_txt.Location = new System.Drawing.Point(71, 61);
+            this.showTime_txt.Name = "showTime_txt";
+            this.showTime_txt.Size = new System.Drawing.Size(200, 76);
+            this.showTime_txt.TabIndex = 0;
+            this.showTime_txt.Text = "24:00";
+            this.showTime_txt.TextChanged += new System.EventHandler(this.showTime_txt_TextChanged);
+            this.showTime_txt.Click += new System.EventHandler(this.showTime_txt_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(828, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 76);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start Clock";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.showTime_txt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1002, 229);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.showTime_txt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label showTime_txt;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ AtomicTime/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Cache;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AtomicTime
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static DateTime GetNistTime()
+        {
+            DateTime dateTime = DateTime.MinValue;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://nist.time.gov/actualtime.cgi?lzbc=siqm9b");
+            request.Method = "GET";
+            request.Accept = "text/html, application/xhtml+xml, */*";
+            request.UserAgent = "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.CachePolicy = new RequestCachePolicy(RequestCacheLevel.NoCacheNoStore); //No caching
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                StreamReader stream = new StreamReader(response.GetResponseStream());
+                string html = stream.ReadToEnd();//<timestamp time=\"1395772696469995\" delay=\"1395772696469995\"/>
+                string time = Regex.Match(html, @"(?<=\btime="")[^""]*").Value;
+                double milliseconds = Convert.ToInt64(time) / 1000.0;
+                dateTime = new DateTime(1970, 1, 1).AddMilliseconds(milliseconds).ToLocalTime();
+            }
+
+            return dateTime;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            showTime_txt.Text = GetNistTime().ToString();
+        }
+
+        private void showTime_txt_Click(object sender, EventArgs e)
+        {
+            while (true)
+            {
+                showTime_txt.Text = GetNistTime().ToString();
+                wait(1);
+            }
+        }
+        public void wait(int min)
+        {
+            System.Windows.Forms.Timer timer1 = new System.Windows.Forms.Timer();
+            if (min == 0 || min < 0) return;
+            //Console.WriteLine("start wait timer");
+            timer1.Interval = min;
+            timer1.Enabled = true;
+            timer1.Start();
+            timer1.Tick += (s, e) =>
+            {
+                timer1.Enabled = false;
+                timer1.Stop();
+                //Console.WriteLine("stop wait timer");
+            };
+            while (timer1.Enabled)
+            {
+                Application.DoEvents();
+            }
+        }
+
+        private void showTime_txt_TextChanged(object sender, EventArgs e)
+        {
+                showTime_txt.Text = GetNistTime().ToString();
+        }
+    }
+}
